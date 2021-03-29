3e9bfddfdb903ef37228388d92af852b9e425fb7
jgothan <jgothan@us.ibm.com>
2021-03-29 14:42:00 -0500

    updated

---
+++ MobilePOApp/MobilePOAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobilePOAppApp.swift
+//  MobilePOApp
+//
+//  Created by Jaishankar Gothandaraman on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct MobilePOAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
